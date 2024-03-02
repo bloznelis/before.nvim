802b74a1ab91c97beee156abf944ebce9319aed7
Lukas Bloznelis <lukas.bloznelis@gmail.com>
2024-03-02 20:36:00 +0200

    feat: same line checks to improve UX

--- lua/before.lua
+++ lua/before.lua
@@ -4,8 +4,8 @@ local M = {}
 M.edit_locations = {}
 M.cursor = 1
 
-local function is_position_within_bounds(line)
-  local total_lines = vim.api.nvim_buf_line_count(0)
+local function is_position_within_bounds(bufnr, line)
+  local total_lines = vim.api.nvim_buf_line_count(bufnr)
   if line < 1 or line > total_lines then
     return false
   end
@@ -24,18 +24,36 @@ function M.track_edit()
   local bufnr = vim.api.nvim_get_current_buf()
   local pos = vim.api.nvim_win_get_cursor(0) -- 0 gets the current window
   local location = { bufnr = bufnr, line = pos[1], col = pos[2] }
+  local prev_location = M.edit_locations[#M.edit_locations]
 
-  M.edit_locations[#M.edit_locations + 1] = location
-  M.cursor = #M.edit_locations
+  if prev_location then
+    if location.line ~= prev_location.line then --should check for same buffer only
+      M.edit_locations[#M.edit_locations + 1] = location
+      M.cursor = #M.edit_locations
+    end
+  else
+    M.edit_locations[#M.edit_locations + 1] = location
+    M.cursor = #M.edit_locations
+  end
 end
 
 function M.jump_to_prev_edit()
   if #M.edit_locations > 0 then
+    local bufnr = vim.api.nvim_get_current_buf()
+    local pos = vim.api.nvim_win_get_cursor(0) -- 0 gets the current window
+    local current = { bufnr = bufnr, line = pos[1], col = pos[2] }
     local location = M.edit_locations[M.cursor]
-    if location and vim.api.nvim_buf_is_loaded(location.bufnr) then
-      dec_cursor()
 
-      if is_position_within_bounds(location.line) then
+    -- If previous edit location is the same as current cursor, try a later one.
+    -- This accommodates for cursor still being in a just-edited-line.
+    if current.line == location.line then
+      location = M.edit_locations[M.cursor - 1]
+    end
+
+    dec_cursor()
+
+    if location and vim.api.nvim_buf_is_loaded(location.bufnr) then
+      if is_position_within_bounds(location.bufnr, location.line) then
         vim.api.nvim_win_set_buf(0, location.bufnr)
         vim.api.nvim_win_set_cursor(0, { location.line, location.col })
       end
