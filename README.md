51491a050f37adf523dc54406188e49c8a4239c2
Lukas Bloznelis <lukas.bloznelis@gmail.com>
2024-03-04 22:01:33 +0200

    various improvements

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # before.nvim
-
 ## Purpose
 Track edit locations and jump back to them across buffers.
 
@@ -8,21 +7,16 @@ Track edit locations and jump back to them across buffers.
 ```lua
 {
   "bloznelis/before.nvim",
-  config = function ()
-    local before = require('before')
+  opts = {
+      -- How many edit locations to store in memory (default: 10)
+      historySize = 10,
 
-    vim.keymap.set('n', 'g[', function()
-      before.jump_to_prev_edit()
-    end, {})
-
-    before.setup()
-  end
+      mapping = {
+        -- Should plugin register default keymaps on setup (default: false)
+        registerDefaults = true,
+        -- Keymap to jump to previous edit location (default: <C-h>)
+        jumpToPreviousEdit = "<C-h>"
+      }
+    }
 }
 ```
-
-## To-Do
-* Prune location list. User shouldn't click "go-back" and go nowhere because last edit is at the same
-location cursor now.
-  * This is quite rare now, but still sometimes happens due to some edgecase. Probably need to loop around the list to find the next hop.
-* Limit edit location list size (make it configurable)
-* Maybe better event to listen to would be [TextChanged](https://neovim.io/doc/user/autocmd.html#TextChanged)https://neovim.io/doc/user/autocmd.html#TextChanged, instead of BufWrite, because if cursor is moved away from the actual edit when the write happens it will record a wrong location.
--- lua/before.lua
+++ lua/before.lua
@@ -1,40 +1,48 @@
 local M = {}
 
--- edit locations stack
 M.edit_locations = {}
 M.cursor = 1
 M.max_entries = 5
 
-local function is_position_within_bounds(bufnr, line)
+local function withinBounds(bufnr, line)
   local total_lines = vim.api.nvim_buf_line_count(bufnr)
-  if line < 1 or line > total_lines then
-    return false
-  end
 
-  return true
+  return line > 1 and line < total_lines
 end
 
-local function dec_cursor()
-  M.cursor = M.cursor - 1
-  if M.cursor < 1 then
-    M.cursor = #M.edit_locations
-  end
+local function bufvalid(bufnr)
+  return vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_is_valid(bufnr)
+end
+
+local function sameLine(thisLocation, thatLocation)
+  return thisLocation.line == thatLocation.line and thisLocation.bufnr == thatLocation.bufnr
+end
+
+local function isNormalBuffer(bufnr)
+  return vim.api.nvim_buf_get_option(bufnr, 'buftype') == ''
+end
+
+local function shouldRemove(location)
+  return not bufvalid(location.bufnr) or not withinBounds(location.bufnr, location.line) or
+      not isNormalBuffer(location.bufnr)
 end
 
 function M.track_edit()
   local bufnr = vim.api.nvim_get_current_buf()
-  local pos = vim.api.nvim_win_get_cursor(0) -- 0 gets the current window
+  local pos = vim.api.nvim_win_get_cursor(0)
   local location = { bufnr = bufnr, line = pos[1], col = pos[2] }
   local prev_location = M.edit_locations[#M.edit_locations]
 
-  if prev_location then
-    if location.line ~= prev_location.line then --should check for same buffer only
+  if isNormalBuffer(bufnr) then
+    if prev_location then
+      if not sameLine(location, prev_location) then
+        M.edit_locations[#M.edit_locations + 1] = location
+        M.cursor = #M.edit_locations
+      end
+    else
       M.edit_locations[#M.edit_locations + 1] = location
       M.cursor = #M.edit_locations
     end
-  else
-    M.edit_locations[#M.edit_locations + 1] = location
-    M.cursor = #M.edit_locations
   end
 
   if #M.edit_locations > M.max_entries then
@@ -43,43 +51,75 @@ function M.track_edit()
   end
 end
 
+local function findJump(currentLocation)
+  local localCursor = M.cursor
+  local lookBackAmount = M.cursor
+  for i = 1, lookBackAmount do
+    localCursor = localCursor - i
+    local location = M.edit_locations[localCursor]
+
+    if location and shouldRemove(location) then
+      table.remove(M.edit_locations, localCursor)
+    else
+      if location and not sameLine(currentLocation, location) then
+        M.cursor = localCursor
+        return location
+      end
+    end
+  end
+
+  local fallbackLocation = M.edit_locations[#M.edit_locations]
+  if fallbackLocation and shouldRemove(fallbackLocation) then
+    table.remove(M.edit_locations, #M.edit_locations)
+  else
+    M.cursor = #M.edit_locations
+    return fallbackLocation
+  end
+end
+
 function M.jump_to_prev_edit()
   if #M.edit_locations > 0 then
     local bufnr = vim.api.nvim_get_current_buf()
-    local pos = vim.api.nvim_win_get_cursor(0) -- 0 gets the current window
+    local pos = vim.api.nvim_win_get_cursor(0)
     local current = { bufnr = bufnr, line = pos[1], col = pos[2] }
-    local location_cursor = M.cursor
-    local location = M.edit_locations[location_cursor]
 
-    if location then
-      -- If previous edit location is the same as current cursor, try a later one.
-      -- This accommodates for cursor still being in a just-edited-line.
-      if current.line == location.line then
-        location_cursor = M.cursor - 1
-        location = M.edit_locations[location_cursor]
-      end
+    local newLocation = findJump(current)
 
-      dec_cursor()
-
-      if location and vim.api.nvim_buf_is_loaded(location.bufnr) and is_position_within_bounds(location.bufnr, location.line) then
-        vim.api.nvim_win_set_buf(0, location.bufnr)
-        vim.api.nvim_win_set_cursor(0, { location.line, location.col })
-      else
-        table.remove(M.edit_locations, location_cursor)
-      end
+    if newLocation then
+      vim.api.nvim_win_set_buf(0, newLocation.bufnr)
+      vim.api.nvim_win_set_cursor(0, { newLocation.line, newLocation.col })
     end
   else
     print("No previous edit locations stored.")
   end
 end
 
-function M.setup()
-  vim.cmd([[
-    augroup EditTracker
-      autocmd!
-      autocmd BufWrite * lua require('before').track_edit()
-    augroup END
-  ]])
+M.defaults = {
+  historySize = 10,
+
+  mapping = {
+    registerDefaults = true,
+    jumpToPreviousEdit = "<C-h>"
+  }
+}
+
+function M.setup(opts)
+  opts = vim.tbl_deep_extend("force", M.defaults, opts)
+
+  M.max_entries = opts.historySize
+
+  if opts.mapping.registerDefaults then
+    vim.keymap.set('n', opts.mapping.jumpToPreviousEdit, function()
+      require('before').jump_to_prev_edit()
+    end, {})
+  end
+
+  vim.api.nvim_create_autocmd({ "TextChanged", "InsertEnter" }, {
+    pattern = "*",
+    callback = function()
+      require('before').track_edit()
+    end,
+  })
 end
 
 return M
